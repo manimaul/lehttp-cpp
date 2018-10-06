019e9aa93c40613d4d9ed2412f1fa065b9155712
Willard Kamp <manimaul@gmail.com>
2018-10-06 16:56:30 -0700

    adding google tests and python integration tests (#4)
    * add gtests
    
    * test config yml format
    
    * add git to docker builder image for CI
    
    * building as shared lib
    
    * lehttp library installation
    
    integration tests
    
    * install example
    
    * test config

---
+++ py_int_test/int_test.py
@@ -0,0 +1,25 @@
+import os.path as path
+import unittest
+from unsafe_client import http_get, http_post, Response
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+    # @skip
+    def test_get_foo(self):
+        response = http_get(url='http://localhost:8181/foo')
+        self.assertEqual("hello path GET foo", response.body)
+        self.assertEqual("there", response.headers['hi'])
+        self.assertEqual(200, response.status)
+
+    # @skip
+    def test_post_foo(self):
+        response = http_post(url='http://localhost:8181/foo', data={})
+        self.assertEqual("hello path POST foo", response.body)
+        self.assertEqual("there", response.headers['hi'])
+        self.assertEqual(200, response.status)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/HttpServerTest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include <math.h>
+#include <cstdio>
+#include <thread>
+#include <cstdlib>
+
+using namespace std;
+
+TEST(HttpServerTest, TestWorksTest) {
+    EXPECT_TRUE(true);
+}
